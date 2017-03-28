f50c0c5c606348f01636fe0fa2ef8bc217d0e13f
vakoz <vakoz2@wp.pl>
2017-03-28 22:26:02 +0200

    last

--- scripts/pg_geoQuery3.sql
+++ scripts/pg_geoQuery3.sql
@@ -1,4 +1,5 @@
-SELECT "Date", "Primary Type", "Description", "Location Description", "Arrest"
+SELECT "Date", "Primary Type", "Description", "Location Description", "Arrest", "Longitude", "Latitude"
 FROM crimes 
 WHERE "Primary Type" LIKE '%THEFT%' AND geom &&
 ST_MakeEnvelope(-87.93834686279295, 41.95693703889415, -87.87483215332031, 42.0064481470799, 4326)
+ORDER BY "Longitude", "Latitude", "Date"
